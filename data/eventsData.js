3cf45f249bad26df590f8cd581cd4948de1a16c0
Ramon Berghorn <ramonhoffmann@ymail.com>
2026-03-04 18:08:43 +0100

    Add eventsData.js with financial events

---
+++ data/eventsData.js
@@ -0,0 +1,23 @@
+export const events = [
+
+{
+ text:"Waschmaschine kaputt (-350€)",
+ cost:-350
+},
+
+{
+ text:"Nebenjob gefunden (+200€)",
+ gain:200
+},
+
+{
+ text:"Auto Reparatur (-600€)",
+ cost:-600
+},
+
+{
+ text:"Geburtstagsgeld (+150€)",
+ gain:150
+}
+
+]
