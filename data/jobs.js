3da79dcdf58b8af4b2760a88232695f834d37a27
Ramon Berghorn <ramonhoffmann@ymail.com>
2026-03-04 18:07:58 +0100

    Add job salary data for various fields

---
+++ data/jobs.js
@@ -0,0 +1,29 @@
+export const jobs = {
+
+  it:{
+    ausbildung:2300,
+    studium:3500,
+    job:3200
+  },
+
+  handwerk:{
+    ausbildung:2500,
+    job:2800
+  },
+
+  pflege:{
+    ausbildung:2400,
+    job:2600
+  },
+
+  buero:{
+    ausbildung:2200,
+    job:2500
+  },
+
+  einzelhandel:{
+    ausbildung:2100,
+    job:2300
+  }
+
+}
